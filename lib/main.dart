b7319372c14bdc8a9ab2748504c2715c23f0a0da
Frenz Luiz <frenzluiz39@gmail.com>
2024-04-22 23:13:27 +0700

    mengubah code untuk menciptakan provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:clairvoyant_tubes/screens/login.dart';
+import 'package:clairvoyant_tubes/screens/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => UserProvider(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const Login(),
-      theme: ThemeData(useMaterial3: false),
-      debugShowCheckedModeBanner: false,
+      title: 'Clairvoyant',
+      home: Login(),
     );
   }
 }
