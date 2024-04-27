b4fc942989a121095236eaee003f27a3b748dcbc
Justin Kosasi <221110323@students.mikroskil.ac.id>
2024-04-27 12:07:43 +0700

    Memodifikasi page chat.dart dan menambahkan page detail_chat.dart pada folder screens

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,168 +1,191 @@
 import 'package:flutter/material.dart';
+import 'package:clairvoyant_tubes/screens/detail_chat.dart';
 
 class ChatPage extends StatelessWidget {
+  List imgs = [
+    "doctor1.jpg",
+    "doctor2.jpg",
+    "doctor3.jpg",
+    "doctor4.jpg",
+    "doctor5.jpg",
+    "doctor6.jpg",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            'CHAT DOKTER & BELI',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 20,
             ),
-          ), 
-          const SizedBox(height: 16),
-          const Text(
-            'OBATNYA MUDAH, BIAR FOKUS LEBARAN LAGI',
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey,
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text("Messages",
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  )),
             ),
-          ),
-          const SizedBox(height: 16),
-          Row(
-            children: [
-              Expanded(
-                child: InkWell(
-                  onTap: () {
-                    // Add code here to navigate to the chat dokter page.
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(color: Colors.blue),
-                    ),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.chat,
-                          size: 24,
-                          color: Colors.blue,
-                        ),
-                        SizedBox(width: 8),
-                        Text(
-                          'CHAT DOKTER',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.blue,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: InkWell(
-                  onTap: () {
-                    // Add code here to navigate to the beli obat page.
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: Colors.blue,
-                    ),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'BELI OBAT',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 16),
-          const Text(
-            'CHAT DOKTER HEMAT s.d. 200+ GRATIS ONGKIR',
-            style: TextStyle(
-              fontSize: 16,
-              color: Colors.grey,
+            const SizedBox(
+              height: 20,
             ),
-          ),
-          const SizedBox(height: 16),
-          Row(
-            children: [
-              Expanded(
-                child: InkWell(
-                  onTap: () {
-                    // Add code here to apply the chatmudik kupon.
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(color: Colors.blue),
-                    ),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Kupon Chatmudik',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.blue,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15),
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 10,
+                        spreadRadius: 2,
+                      )
+                    ]),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: 300,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            hintText: "Search",
+                            border: InputBorder.none,
                           ),
                         ),
-                      ],
+                      ),
                     ),
-                  ),
+                    const Icon(Icons.search, color: Color(0xFF7165D6))
+                  ],
                 ),
               ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: InkWell(
-                  onTap: () {
-                    // Add code here to navigate to the chat dokter history page.
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      border: Border.all(color: Colors.blue),
-                    ),
-                    child: const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.history,
-                          size: 24,
-                          color: Colors.blue,
-                        ),
-                        SizedBox(width: 8),
-                        Text(
-                          'Riwayat Chat',
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.blue,
+            ),
+            const SizedBox(height: 20),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                "Active Now",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 90,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 6,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 12),
+                      width: 65,
+                      height: 65,
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                            )
+                          ]),
+                      child: Stack(
+                        textDirection: TextDirection.rtl,
+                        children: [
+                          Center(
+                            child: Container(
+                              height: 65,
+                              width: 65,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(30),
+                                child: Image.asset(
+                                  "lib/images/${imgs[index]}",
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                          Container(
+                            margin: const EdgeInsets.all(2),
+                            padding: const EdgeInsets.all(3),
+                            height: 20,
+                            width: 20,
+                            decoration: const BoxDecoration(
+                              color: Colors.white,
+                              shape: BoxShape.circle,
+                            ),
+                            child: Container(
+                              decoration: const BoxDecoration(
+                                color: Colors.green,
+                                shape: BoxShape.circle,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  }),
+            ),
+            const SizedBox(height: 10),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                "Recent Chat",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-            ],
-          ),
-        ],
+            ),
+            const SizedBox(height: 5),
+            ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 6,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => DetailChat(
+                                  gambar: "lib/images/${imgs[index]}",
+                                  name: "Doctor ${index + 1}",
+                                )));
+                      },
+                      leading: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage(
+                          "lib/images/${imgs[index]}",
+                        ),
+                      ),
+                      title: Text(
+                        "Doctor ${index + 1}",
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      subtitle: const Text(
+                        "Hello, Doctor",
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 16, color: Colors.black54),
+                      ),
+                      trailing: const Text(
+                        "12.30",
+                        style: TextStyle(fontSize: 15, color: Colors.black54),
+                      ),
+                    ),
+                  );
+                })
+          ],
+        ),
       ),
     );
   }
 }
-
-
