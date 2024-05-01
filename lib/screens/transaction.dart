d0eddc22834d4722790fc6c71db75617875121ae
Frenz Luiz <frenzluiz39@gmail.com>
2024-05-01 11:55:31 +0700

    menambah kode untuk histori pembayaran

--- lib/screens/transaction.dart
+++ lib/screens/transaction.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
-class TransactionHistory extends StatelessWidget {
-  const TransactionHistory({super.key});
+class TransactionHistory extends StatefulWidget {
+  final List<Map> selectedItems;
+
+  const TransactionHistory({Key? key, required this.selectedItems})
+      : super(key: key);
 
   @override
+  _TransactionHistoryState createState() => _TransactionHistoryState();
+}
+
+class _TransactionHistoryState extends State<TransactionHistory> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transaction History'),
+      ),
+      body: ListView.builder(
+        itemCount: widget.selectedItems.length,
+        itemBuilder: (context, index) {
+          final item = widget.selectedItems[index];
+          return ListTile(
+            title: Text(item['name'] ?? ''),
+            subtitle: Text('Quantity: ${item['amount']}'),
+          );
+        },
+      ),
+    );
   }
 }
