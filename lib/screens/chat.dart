bda7f5e2fdcc452b97291cd536627da50be03a41
Frenz Luiz <frenzluiz39@gmail.com>
2024-05-29 23:00:24 +0700

    menambah modal bottom sheet di bagian chat dokter

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -11,41 +11,77 @@ class ChatPage extends StatelessWidget {
     "doctor6.jpg",
   ];
 
+  void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.chat),
+                title: Text('New Chat'),
+                onTap: () {
+                  // Handle starting a new chat
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Chat Settings'),
+                onTap: () {
+                  // Handle chat settings
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Messages"),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 102, 03, 219),
+      ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(
-              height: 20,
-            ),
+            const SizedBox(height: 20),
             const Padding(
               padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text("Messages",
-                  style: TextStyle(
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  )),
+              child: Text(
+                "Messages",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
+            const SizedBox(height: 20),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 15),
               child: Container(
                 padding: const EdgeInsets.symmetric(horizontal: 15),
                 decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 10,
-                        spreadRadius: 2,
-                      )
-                    ]),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 10,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -61,7 +97,7 @@ class ChatPage extends StatelessWidget {
                         ),
                       ),
                     ),
-                    const Icon(Icons.search, color: Color(0xFF7165D6))
+                    const Icon(Icons.search, color: Color(0xFF7165D6)),
                   ],
                 ),
               ),
@@ -78,60 +114,62 @@ class ChatPage extends StatelessWidget {
             SizedBox(
               height: 90,
               child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 6,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 12),
-                      width: 65,
-                      height: 65,
-                      decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black12,
-                              spreadRadius: 2,
-                              blurRadius: 10,
-                            )
-                          ]),
-                      child: Stack(
-                        textDirection: TextDirection.rtl,
-                        children: [
-                          Center(
-                            child: Container(
-                              height: 65,
-                              width: 65,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(30),
-                                child: Image.asset(
-                                  "lib/images/${imgs[index]}",
-                                  fit: BoxFit.cover,
-                                ),
+                scrollDirection: Axis.horizontal,
+                itemCount: 6,
+                shrinkWrap: true,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 12),
+                    width: 65,
+                    height: 65,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          spreadRadius: 2,
+                          blurRadius: 10,
+                        ),
+                      ],
+                    ),
+                    child: Stack(
+                      textDirection: TextDirection.rtl,
+                      children: [
+                        Center(
+                          child: Container(
+                            height: 65,
+                            width: 65,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(30),
+                              child: Image.asset(
+                                "lib/images/${imgs[index]}",
+                                fit: BoxFit.cover,
                               ),
                             ),
                           ),
-                          Container(
-                            margin: const EdgeInsets.all(2),
-                            padding: const EdgeInsets.all(3),
-                            height: 20,
-                            width: 20,
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(2),
+                          padding: const EdgeInsets.all(3),
+                          height: 20,
+                          width: 20,
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Container(
                             decoration: const BoxDecoration(
-                              color: Colors.white,
+                              color: Colors.green,
                               shape: BoxShape.circle,
                             ),
-                            child: Container(
-                              decoration: const BoxDecoration(
-                                color: Colors.green,
-                                shape: BoxShape.circle,
-                              ),
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  }),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
             ),
             const SizedBox(height: 10),
             const Padding(
@@ -143,48 +181,57 @@ class ChatPage extends StatelessWidget {
             ),
             const SizedBox(height: 5),
             ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: 6,
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 5),
-                    child: ListTile(
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => DetailChat(
-                                  gambar: "lib/images/${imgs[index]}",
-                                  name: "Doctor ${index + 1}",
-                                )));
-                      },
-                      leading: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: AssetImage(
-                          "lib/images/${imgs[index]}",
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: 6,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 5),
+                  child: ListTile(
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => DetailChat(
+                            gambar: "lib/images/${imgs[index]}",
+                            name: "Doctor ${index + 1}",
+                          ),
                         ),
-                      ),
-                      title: Text(
-                        "Doctor ${index + 1}",
-                        style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: const Text(
-                        "Hello, Doctor",
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(fontSize: 16, color: Colors.black54),
-                      ),
-                      trailing: const Text(
-                        "12.30",
-                        style: TextStyle(fontSize: 15, color: Colors.black54),
+                      );
+                    },
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage(
+                        "lib/images/${imgs[index]}",
                       ),
                     ),
-                  );
-                })
+                    title: Text(
+                      "Doctor ${index + 1}",
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: const Text(
+                      "Hello, Doctor",
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 16, color: Colors.black54),
+                    ),
+                    trailing: const Text(
+                      "12.30",
+                      style: TextStyle(fontSize: 15, color: Colors.black54),
+                    ),
+                  ),
+                );
+              },
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showBottomSheet(context),
+        child: Icon(Icons.more_horiz),
+        backgroundColor: Color.fromARGB(255, 102, 03, 219),
       ),
     );
   }
