cb817a004d2f604b716ddd4329346284527d9ede
Frenz Luiz <frenzluiz39@gmail.com>
2024-05-29 23:11:54 +0700

    menambah provider tema

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:clairvoyant_tubes/screens/login.dart';
 import 'package:clairvoyant_tubes/screens/provider.dart';
+import 'package:clairvoyant_tubes/screens/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (_) => UserProvider(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => UserProvider()),
+        ChangeNotifierProvider(
+            create: (_) => ThemeProvider()), // Add ThemeProvider here
+      ],
       child: MyApp(),
     ),
   );
@@ -15,10 +20,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Clairvoyant',
-      home: Login(),
-      debugShowCheckedModeBanner: false,
+    return Consumer<ThemeProvider>(
+      builder: (context, themeProvider, child) {
+        return MaterialApp(
+          title: 'Clairvoyant',
+          theme: ThemeData.light(),
+          darkTheme: ThemeData.dark(),
+          themeMode:
+              themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
+          home: Login(),
+          debugShowCheckedModeBanner: false,
+        );
+      },
     );
   }
 }
